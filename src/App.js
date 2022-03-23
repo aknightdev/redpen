b18f52d2a411badf3c40adaafaa39c6405a259ea
magentodevelopment2019 <magentodevelopment2019@gmail.com>
2022-03-23 15:11:04 +0530

    Changes 23rd Mar 2022

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import 'assets/css/style.css';
+import 'assets/css/web_style.css';
+import 'assets/css/responsive-web.css';
+import 'assets/css/responsive.css';
+import Main from 'components/Main/Main.js';
+import Header from 'components/Header/Header.js';
+import Footer from 'components/Footer/Footer.js';
+import fontawesome from '@fortawesome/fontawesome'
+import { faCheckSquare, faCoffee } from '@fortawesome/fontawesome-free-solid'
+fontawesome.library.add(faCheckSquare, faCoffee);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Main projectData="1" />
+      <Footer />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
+import { logger } from 'redux-logger'
+import reducer from './reducers'
+const store = createStore(
+	reducer,
+	applyMiddleware(thunk, logger)
+)
+ReactDOM.render((
+	<Provider store={store}>
+	  <BrowserRouter>
+	    <App />
+	  </BrowserRouter>
+  	</Provider>
+), document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
