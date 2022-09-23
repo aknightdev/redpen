a6c909c49de1edcdbfb0828ff53f895f0325938d
aknightdev <developer@knightagency.com.au>
2022-09-23 16:22:35 +0530

    Changes

---
+++ src/views/Invoices/Invoices.js
@@ -0,0 +1,74 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import {Typeahead} from 'react-bootstrap-typeahead';
+import 'react-bootstrap-typeahead/css/Typeahead.css';
+import Modal from 'components/Modal/Modal.js';
+import $ from 'jquery';
+import { config } from 'Constants.js';
+import {getRandomColor,createImageFromInitials} from 'components/Utils.js'
+import Moment from 'react-moment';
+
+export default class Invoices extends React.Component {
+	constructor(props) {
+	    super(props);
+	    this.state = {success:false, payments:[]};
+	    this.authUser = window.localStorage.getItem('auth_user')==null?{id:null,name:null,email:null}:JSON.parse(window.localStorage.getItem('auth_user'));
+	}
+	componentDidMount(){
+		if (this.authUser.id!=null) {
+			fetch(config.url.API_URL+"payments", {
+			  	method: "POST",
+			  	body: JSON.stringify({user_id:this.authUser.id}),
+		  		headers: {
+			        'Content-Type': 'application/json'
+			    }
+			}).then(function (response) {
+	            return response.json();
+		    }).then( (resp) => {
+		    	this.setState({payments: resp});
+		    });
+		}
+	}
+	render() {
+		var success = null, button=null;
+		if (this.state.success) {
+			success = <aside className="success-balloon "><span>Okay then. We’ve emailed them the good news. </span><strong>Add another?</strong></aside>;
+		}
+		if (this.state.validEmail) {
+			button = <button className="new-button primary light email-button" onClick={this.addTeammate}>Invite team member</button>;
+		}
+		return (
+			<div className="page_body">
+			<div className="page_wrapper">
+			<div className="container2 home">
+				<div className="team_row">
+					<div className="team">
+					<h2>Invoices</h2>
+					<div className="teammates-list">
+						<table>
+							<thead>
+								<tr>
+									<th width="60%">Transaction ID</th>
+									<th width="20%">Amount</th>
+									<th>Date</th>
+								</tr>
+							</thead>
+							<tbody>
+								{this.state.payments.map((payment,key) => (
+								<tr key={key}>
+									<td>{payment.transaction_id}</td>
+									<td>$ {parseFloat(payment.amount).toFixed(2)}</td>
+									<td><Moment format="DD MMM YY hh:mma">{payment.created}</Moment></td>
+								</tr>
+								))}
+							</tbody>
+						</table>
+					</div>
+					</div>
+				</div>
+			</div>
+			</div>
+			</div>
+		);
+	}
+}
